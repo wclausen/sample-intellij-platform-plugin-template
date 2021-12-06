dced062f0c1ef3362b91419247ddea8f5c407838
GitHub Action <action@github.com>
2021-12-06 21:24:44 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "sample-intellij-platform-plugin-template"
---
+++ src/main/kotlin/com/github/wclausen/sampleintellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wclausen.sampleintellijplatformplugintemplate.services
+
+import com.github.wclausen.sampleintellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
